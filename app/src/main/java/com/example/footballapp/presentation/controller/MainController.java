febad2d538d3e91466be8b64aeb307da390172aa
rayanecherouali <rcherouali@hotmail.fr>
2020-05-12 16:20:23 +0200

    Add MVC architecture for MainScreen

---
+++ app/src/main/java/com/example/footballapp/presentation/controller/MainController.java
@@ -0,0 +1,121 @@
+package com.example.footballapp.presentation.controller;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.footballapp.Constants;
+import com.example.footballapp.data.PsgApi;
+import com.example.footballapp.presentation.model.RestFootballResponse;
+import com.example.footballapp.presentation.model.psgTeam;
+import com.example.footballapp.presentation.view.MainActivity;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainController {
+
+
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
+    private MainActivity view;
+
+
+    public MainController(MainActivity mainActivity, Gson gson, SharedPreferences sharedPreferences) {
+        this.view = mainActivity;
+        this.gson = gson;
+        this.sharedPreferences = sharedPreferences;
+
+
+        }
+        public void onStart(){
+
+            List<psgTeam> psgTeamList = getDataFromCache();
+            if (psgTeamList != null) {
+                view.showList(psgTeamList);
+            } else {
+                makeApiCall();
+
+            }
+
+
+        }
+
+
+    public void makeApiCall(){
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        PsgApi psgApi = retrofit.create(PsgApi.class);
+
+        Call<RestFootballResponse> call = psgApi.getFootballResponse();
+        call.enqueue(new Callback<RestFootballResponse>() {
+            @Override
+            public void onResponse(Call<RestFootballResponse> call, Response<RestFootballResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<psgTeam> psgTeamList = response.body().getSquad();
+                    saveList(psgTeamList);
+                    view.showList(psgTeamList);
+                } else {
+                    view.showError();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RestFootballResponse> call, Throwable t) {
+
+                view.showError();
+            }
+        });
+    }
+
+    private void saveList(List<psgTeam> psgTeamList) {
+        String jsonString = gson.toJson(psgTeamList);
+
+        sharedPreferences
+                .edit()
+                .putString(Constants.KEY_psgTEAM_LIST , jsonString)
+                .apply();
+
+    }
+
+    private List<psgTeam> getDataFromCache() {
+        String jsonpsgTeamList = sharedPreferences.getString( Constants.KEY_psgTEAM_LIST, null);
+
+        if (jsonpsgTeamList == null) {
+            return null;
+        } else {
+            Type listType = new TypeToken<List<psgTeam>>() {
+            }.getType();
+            return gson.fromJson(jsonpsgTeamList, listType);
+        }
+    }
+
+
+    public void onItemClick(psgTeam psgTeam){
+
+
+        }
+        public void onButton1ACLICK() {
+
+
+        }
+        public void onButton1BCLICK(){
+
+
+        }
+
+
+}
--- app/src/main/java/com/example/footballapp/presentation/view/MainActivity.java
+++ app/src/main/java/com/example/footballapp/presentation/view/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.example.footballapp.Constants;
 import com.example.footballapp.R;
 import com.example.footballapp.data.PsgApi;
+import com.example.footballapp.presentation.controller.MainController;
 import com.example.footballapp.presentation.model.RestFootballResponse;
 import com.example.footballapp.presentation.model.psgTeam;
 import com.google.gson.Gson;
@@ -29,12 +30,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final String BASE_URL = "https://raw.githubusercontent.com/rayanecher/FootballApp/master/";
+
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-    private SharedPreferences sharedPreferences;
-    private Gson gson;
+    private MainController controller;
 
 
     @Override
@@ -42,33 +42,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sharedPreferences = getSharedPreferences("application_esiea", Context.MODE_PRIVATE);
-        gson = new GsonBuilder()
-                .setLenient()
-                .create();
+        controller = new MainController(
+                this,
+               new GsonBuilder()
+                        .setLenient()
+                        .create(),
+                getSharedPreferences("application_esiea", Context.MODE_PRIVATE)
 
-        List<psgTeam> psgTeamList = getDataFromCache();
-        if (psgTeamList != null) {
-            showList(psgTeamList);
-        } else {
-            makeApiCall();
 
-        }
-    }
-    private List<psgTeam> getDataFromCache() {
-        String jsonpsgTeamList = sharedPreferences.getString( Constants.KEY_psgTEAM_LIST, null);
+        );
+        controller.onStart();
 
-        if (jsonpsgTeamList == null) {
-            return null;
-        } else {
-            Type listType = new TypeToken<List<psgTeam>>() {
-            }.getType();
-            return gson.fromJson(jsonpsgTeamList, listType);
-        }
     }
 
 
-    private void showList(List<psgTeam> psgTeamList) {
+
+    public void showList(List<psgTeam> psgTeamList) {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         // use a linear layout manager
@@ -82,50 +71,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-    public void makeApiCall(){
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-        PsgApi psgApi = retrofit.create(PsgApi.class);
-
-        Call<RestFootballResponse> call = psgApi.getFootballResponse();
-        call.enqueue(new Callback<RestFootballResponse>() {
-            @Override
-            public void onResponse(Call<RestFootballResponse> call, Response<RestFootballResponse> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    List<psgTeam> psgTeamList = response.body().getSquad();
-                    saveList(psgTeamList);
-                    showList(psgTeamList);
-                } else {
-                    showError();
-
-                }
-            }
-
-                @Override
-                public void onFailure(Call<RestFootballResponse> call, Throwable t) {
-                    showError();
-                }
-        });
-    }
-
-    private void saveList(List<psgTeam> psgTeamList) {
-        String jsonString = gson.toJson(psgTeamList);
-
-        sharedPreferences
-                .edit()
-                .putString(Constants.KEY_psgTEAM_LIST , jsonString)
-                .apply();
-
-    }
-
     public void showError(){
         Toast.makeText(getApplicationContext(), "API ERROR", Toast.LENGTH_SHORT).show();
-
 
     }
 }
